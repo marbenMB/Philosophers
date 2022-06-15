76a382fb74bf1b1e2f1be987ddc9fbf303b3251c
Marouane  Benbajja <marouanebenbajja@gmail.com>
2022-06-15 22:06:07 +0100

    Modification in sem_open

--- philo_bonus/philo_utils.c
+++ philo_bonus/philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: mbenbajj <mbenbajj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 03:11:01 by mbenbajj          #+#    #+#             */
-/*   Updated: 2022/06/15 20:50:35 by mbenbajj         ###   ########.fr       */
+/*   Updated: 2022/06/15 22:05:41 by mbenbajj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ int	data_init(int ac, char **av, t_data **data)
 	(*data)->t_start = 0;
 	sem_unlink("fork");
 	sem_unlink("print");
-	(*data)->fork = sem_open("fork", O_CREAT, S_IRWXU, (*data)->nbr_philo);
-	(*data)->print = sem_open("print", O_CREAT, S_IRWXU, 1);
+	(*data)->fork = sem_open("fork", O_CREAT, 666, (*data)->nbr_philo);
+	(*data)->print = sem_open("print", O_CREAT, 666, 1);
 	return (0);
 }
 
